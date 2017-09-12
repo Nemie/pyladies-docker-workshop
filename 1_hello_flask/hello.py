e5f5e8452e3450d8e5a93d5aac0ed74b7346647a
Giulia Vergottini <hello@railslide.io>
2017-09-12 08:34:44 +0200

    Add initial files for flask example

---
+++ 1_hello_flask/hello.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_pyladies():
+    return 'Hello, pyLadies!'
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 7000)
