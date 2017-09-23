29fc56ae0102c65db825031bd21b93c4be031e33
Giulia Vergottini <hello@railslide.io>
2017-09-23 21:35:14 +0200

    Add db exercise to help folder

---
+++ help/2_hello_db/hello.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from sqlalchemy import create_engine
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_pyladies():
+    return "Hello, pyLadies! I'm a Flask app"
+
+
+@app.route('/db')
+def hello_db():
+    try:
+        db = create_engine('postgresql://postgres:postgres@db:5432/docker_db')
+        result = db.execute('SELECT 1').first()
+        if result and result[0] == 1:
+            return 'Connected to the database \\o/'
+    except Exception:
+        return 'Cannot connect to the database :('
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 7000)
